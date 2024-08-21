261028d8b19c73873aa9ed9ea0f101b4795c0157
unknown <michellemoney2003@gmail.com>
2024-08-21 13:20:44 +0300

    config update

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 BASE_DIR = "./files"
 DOWNLOAD_PATH = "/Downloads/downloaded_file.txt"
 KILOBYTE = 1024 * 1024
+USER = "user"
+PASSWORD = "12345"
+HOST = "127.0.0.1"
+PORT = 8000
 
--- ftp_server.py
+++ ftp_server.py
@@ -1,12 +1,12 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-from config import BASE_DIR
+from config import BASE_DIR, USER, PASSWORD
 
 
 def start_ftp_server() -> None:
     authorizer = DummyAuthorizer()
-    authorizer.add_user("user", "12345", BASE_DIR, perm="elradfmw")
+    authorizer.add_user(USER, PASSWORD, BASE_DIR, perm="elradfmw")
 
     handler = FTPHandler
     handler.authorizer = authorizer
